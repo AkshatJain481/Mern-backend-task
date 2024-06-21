a14d0b0e79101c033339d2abd9b2b52bb3165f12
Akshat <akshatjain481@gmail.com>
2024-06-21 20:05:23 +0530

    bug fixing

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+  origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow all HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+}));
 
 // MongoDB connection
 mongoose.connect('mongodb+srv://akshatjain481:abcdefgh@cluster0.nsi5rbr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
